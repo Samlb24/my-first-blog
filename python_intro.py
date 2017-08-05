de5b02b991f34a428b1c85d54a6c5be8e3b548dd
sam <samlouiseblake@gmail.com>
2017-08-05 14:21:00 +1000

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+print('Hello, Django girls!')
+if 3>2:
+    print('It works!')
+name = 'Sonja'
+if name == 'Sonja':
+    print('Hey mate!')
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
